ce7aafffd78e033863279d5b0886fef9985fef1c
BalaMurugan <bals161616@gmail.com>
2023-04-27 12:16:46 +0530

    getting all expenses api also created

--- src/main/java/com/expense/management/controller/CentralController.java
+++ src/main/java/com/expense/management/controller/CentralController.java
@@ -1,12 +1,10 @@
 package com.expense.management.controller;
 
-import com.expense.management.Request.CentralBaseRequest;
 import com.expense.management.database.entity.Expense;
 import com.expense.management.response.BaseResponse;
 import com.expense.management.service.CentralService;
 import com.expense.management.utils.BaseResponseUtils;
 import lombok.extern.log4j.Log4j2;
-//import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,39 +34,14 @@ public class CentralController {
         }
     }
 
-//    @GetMapping("/get-users")
-//    public BaseResponse getUsers(@RequestBody(required = false) List<String> mobileNoList){
-//        try {
-//            return centralService.getUserDetails(mobileNoList);
-//        }catch (Exception e){
-//            log.info("Error in addUser", e);
-//            return BaseResponseUtils.createErrorBaseResponse();
-//        }
-//    }
-//
-//    @PostMapping("/add-partner")
-//    public BaseResponse addPartner(@RequestBody CentralBaseRequest centralBaseRequest){
-//        try {
-//            log.info("centralBaseRequest - {}", centralBaseRequest);
-//            if(StringUtils.isEmpty(centralBaseRequest.getName()) || StringUtils.isEmpty(centralBaseRequest.getMobile()))
-//                return BaseResponseUtils.createErrorBaseResponse("Username or Mobile must not be empty");
-//
-//            centralService.savePartnerDetails(centralBaseRequest);
-//            return BaseResponseUtils.createSuccessBaseResponse();
-//        }catch (Exception e){
-//            log.info("Error in addPartner", e);
-//            return BaseResponseUtils.createErrorBaseResponse();
-//        }
-//    }
-//
-//    @GetMapping("/get-partners")
-//    public BaseResponse getPartners(@RequestBody(required = false) List<String> mobileNoList){
-//        try {
-//            return centralService.getPartnerDetails(mobileNoList);
-//        }catch (Exception e){
-//            log.info("Error in addUser", e);
-//            return BaseResponseUtils.createErrorBaseResponse();
-//        }
-//    }
+    @GetMapping("/get-expenses")
+    public BaseResponse getExpenses(){
+        try {
+            return centralService.getExpensesDetails();
+        }catch (Exception e){
+            log.info("Error in getExpenses", e);
+            return BaseResponseUtils.createErrorBaseResponse();
+        }
+    }
 
 }
--- src/main/java/com/expense/management/service/CentralService.java
+++ src/main/java/com/expense/management/service/CentralService.java
@@ -2,21 +2,14 @@ package com.expense.management.service;
 
 import com.expense.management.database.dao.ExpenseDao;
 import com.expense.management.database.entity.Expense;
-//import com.passbook.demo.Request.CentralBaseRequest;
-//import com.passbook.demo.database.dao.PartnerDao;
-//import com.passbook.demo.database.dao.UserDao;
-//import com.passbook.demo.database.entity.Partner;
-//import com.passbook.demo.database.entity.User;
-//import com.passbook.demo.response.BaseResponse;
-//import com.passbook.demo.response.CentralResponse;
-//import com.passbook.demo.utils.BaseResponseUtils;
+import com.expense.management.response.BaseResponse;
+import com.expense.management.response.CentralResponse;
+import com.expense.management.utils.BaseResponseUtils;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-
-import java.util.Date;
 import java.util.List;
 
 
@@ -27,72 +20,28 @@ public class CentralService {
     @Autowired
     ExpenseDao expenseDao;
 
-//    @Autowired
-//    PartnerDao partnerDao;
-
     @Async
     public void saveExpenseDetails(Expense expense) {
         expenseDao.save(expense);
     }
 
-//    public BaseResponse getUserDetails(List<String> mobileNoList) {
-//        try {
-//            List<User> users;
-//            if(CollectionUtils.isEmpty(mobileNoList))
-//                users = userDao.findAll();
-//            else
-//                users = userDao.findAllByMobile(mobileNoList);
-//
-//            if(CollectionUtils.isEmpty(users))
-//                return BaseResponseUtils.createNoDataBaseResponse();
-//
-//            return CentralResponse.builder()
-//                    .users(users)
-//                    .es(0)
-//                    .message("success !")
-//                    .status(200)
-//                    .time(System.currentTimeMillis())
-//                    .build();
-//        }catch (Exception e){
-//            log.info("Error in getUserDetails", e);
-//            return BaseResponseUtils.createErrorBaseResponse();
-//        }
-//    }
-//
-//    @Async
-//    public void savePartnerDetails(CentralBaseRequest centralBaseRequest) {
-//        Partner partner = Partner.builder()
-//                .name(centralBaseRequest.getName())
-//                .mobile(centralBaseRequest.getMobile())
-//                .address(centralBaseRequest.getAddress())
-//                .cashInHand(centralBaseRequest.getCash())
-//                .createdAt(new Date())
-//                .updatedAt(new Date())
-//                .build();
-//        partnerDao.save(partner);
-//    }
-//
-//    public BaseResponse getPartnerDetails(List<String> mobileNoList) {
-//        try {
-//            List<Partner> partners;
-//            if(CollectionUtils.isEmpty(mobileNoList))
-//                partners = partnerDao.findAll();
-//            else
-//                partners = partnerDao.findAllByMobile(mobileNoList);
-//
-//            if(CollectionUtils.isEmpty(partners))
-//                return BaseResponseUtils.createNoDataBaseResponse();
-//
-//            return CentralResponse.builder()
-//                    .partners(partners)
-//                    .es(0)
-//                    .message("success !")
-//                    .status(200)
-//                    .time(System.currentTimeMillis())
-//                    .build();
-//        }catch (Exception e){
-//            log.info("Error in getPartnerDetails", e);
-//            return BaseResponseUtils.createErrorBaseResponse();
-//        }
-//    }
+    public BaseResponse getExpensesDetails() {
+        try {
+            List<Expense> expenses = expenseDao.findAll();
+
+            if(CollectionUtils.isEmpty(expenses))
+                return BaseResponseUtils.createNoDataBaseResponse();
+
+            return CentralResponse.builder()
+                    .expenses(expenses)
+                    .es(0)
+                    .message("success !")
+                    .status(200)
+                    .time(System.currentTimeMillis())
+                    .build();
+        }catch (Exception e){
+            log.info("Error in getExpensesDetails", e);
+            return BaseResponseUtils.createErrorBaseResponse();
+        }
+    }
 }
